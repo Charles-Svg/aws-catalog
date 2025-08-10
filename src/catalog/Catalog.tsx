76aa0dc800a7113889fa2775be59b70f86f58f51
Charles-Svg <78506028+Charles-Svg@users.noreply.github.com>
2025-08-10 10:29:51 +0200

    Fix arrows placement

--- src/catalog/Catalog.tsx
+++ src/catalog/Catalog.tsx
@@ -65,77 +65,72 @@ function Catalog({architectures} : Archis) {
 
   return (
   <>
-    <Button
-        variant="ghost"
-        onClick={() => navigate("/list")}
-        className="flex items-center space-x-1 z-10 lg:absolute lg:top-4 lg:left-4"
-      >
-        <ChevronLeft className="h-4 w-4 " />
-        <span className="text-sm">Back to list</span>
-    </Button>
+  <Button
+      variant="ghost"
+      onClick={() => navigate("/list")}
+      className="flex items-center space-x-1 z-10 absolute top-4 left-4"
+    >
+      <ChevronLeft className="h-4 w-4 " />
+      <span className="text-sm">Back to list</span>
+  </Button>
   {(architectures.length !== 0 && !error) &&
-   <div className={`flex flex-col gap-6 p-4 md:p-8 max-w-5xl lg:pt-12mx-auto 
-    transition-transform transition-opacity duration-200 ease-in-out transform  
-      ${isSwiping ? 'scale-[1.02] shadow-lg' : ''} 
-    `}
-        style= {{
+   <div className={
+    `m-auto flex flex-col gap-6 p-4 md:p-8 max-w-5xl lg:pt-12mx-auto transition-opacity duration-200 ease-in-out    
+    `}>
+
+    {/* Flèche Précédent (desktop uniquement) */}
+      {index > 0 && (
+        <button
+        onClick={prev}
+        className="hidden lg:flex absolute lg:left-[5vw] xl:left-[8vw] xxl:left-[12vw] z-50 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full p-2 shadow hover:bg-gray-100"
+        >
+          <ChevronLeft size={24} />
+        </button>
+      )}
+
+      {/* Flèche Suivant (desktop uniquement) */}
+      {index < architectures.length - 1 && (
+        <button
+        onClick={next}
+        className="hidden lg:flex absolute z-50 lg:right-[5vw] xl:right-[8vw] xxl:right-[12vw] top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full p-2 shadow hover:bg-gray-100"
+        >
+          <ChevronRight size={24} />
+        </button>
+      )}
+
+
+      <div style= {{
           transform: `translateX(${translateX}px)`,
           transition: swipeDirection ? 'transform 0.3s ease-out' : undefined,
           opacity: 1 - Math.min(Math.abs(translateX) / 300, 0.5),
         }}
-        onTransitionEnd={() => {setSwipeDirection(null);}}>
-
-
-      <h1 className={`text-2xl md:text-4xl font-bold text-gray-800 text-center `}>
-      {archi["title"]}
-      </h1>
-          {/* Flèche Précédent (desktop uniquement) */}
-          {index > 0 && (
-            <button
-            onClick={prev}
-            className="hidden lg:flex absolute left-4 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full p-2 shadow hover:bg-gray-100"
-            >
-              <ChevronLeft size={24} />
-            </button>
-          )}
-
-          {/* Flèche Suivant (desktop uniquement) */}
-          {index < architectures.length - 1 && (
-            <button
-            onClick={next}
-            className="hidden lg:flex absolute right-4 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full p-2 shadow hover:bg-gray-100"
-            >
-              <ChevronRight size={24} />
-            </button>
-          )}
-
+        onTransitionEnd={() => {setSwipeDirection(null);}}
+        className={`transform ${isSwiping ? 'scale-[1.02] shadow-lg' : ''} transition-transform`}>
           
-          {/* Iframe integration is blocked by cookies on most browsers...
-          <div className="w-full h-full aspect-video">
-          <iframe
-          src={archi["embedUrl"]}
+        <h2 className={`text-xl md:text-2xl font-bold text-gray-800 text-center `}>
+          {archi["title"]}
+        </h2>
+
+        <LucidImageViewer
+          src={`/img/${archi["image"]}`}
           title={archi["title"]}
-          allowFullScreen
-          className="w-full h-full border border-gray-300 rounded-md"
-          id={archi["lucidId"]}
-          />
-          </div> */}
-          <LucidImageViewer
-            src={`/img/${archi["image"]}`}
-            title={archi["title"]}
-          />
+        />
+        
         <div {...swipeHandlers}>
           <div className="text-center text-sm text-gray-500 animate-pulse block lg:hidden">
-             ⬅️ Swipe left or right to navigate ➡️
+              ⬅️ Swipe left or right to navigate ➡️
           </div>
           {/* Description */}
           <div  className="w-full">
-            <h2 className="text-xl font-semibold" style={{marginBottom: "1rem", marginTop: "1rem"}}>Description</h2>
+            <h3 className="text-xl font-semibold" style={{marginBottom: "1rem", marginTop: "1rem"}}>Description</h3>
             <div className="text-gray-700 whitespace-pre-line text-left leading-[1.2]"><MarkdownContent fichier={`description_${archi["id"]}`}/></div>
           </div>
         </div>
-    <MobileRotateNotice />
-    </div>}
+      </div>
+      <MobileRotateNotice />
+    </div>
+    
+    }
     {(error && 
       <div className="text-center ">
         <p className="text-red-500">Erreur sur l'architecture revenez à la page de liste</p>
